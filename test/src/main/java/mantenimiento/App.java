7660f47819b6e815858dfe84adfe52e89587584f
Jens Hoffmann <jens.hoffmann@utp.ac.pa>
2022-10-23 22:49:27 -0500

    Primer commit – Pruebas en Selenium

---
+++ test/src/main/java/mantenimiento/App.java
@@ -0,0 +1,13 @@
+package mantenimiento;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
