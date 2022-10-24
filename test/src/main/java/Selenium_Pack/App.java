4242f2e42f66d1a844965f188e473790c96a9940
Jens Hoffmann <jens.hoffmann@utp.ac.pa>
2022-10-24 01:01:15 -0500

    Tercer commit, Ya añadimos una revision busqueda dentro de amazon para hacer una prueba

--- test/src/main/java/Selenium_Pack/App.java
+++ test/src/main/java/Selenium_Pack/App.java
@@ -2,7 +2,9 @@
 
 package Selenium_Pack;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class App 
 {
@@ -12,7 +14,9 @@ public class App
     System.setProperty("webdriver.chrome.driver","C:\\Chomedriver\\chromedriver.exe");
     WebDriver driver = new ChromeDriver();
     driver.get("https://www.amazon.com/");
-    
+    WebElement buscar = driver.findElement(By.id("twotabsearchtextbox"));
+    buscar.sendKeys("Magic Cards");
+    buscar.submit();
     Thread.sleep(5000);
      driver.quit();
     }
